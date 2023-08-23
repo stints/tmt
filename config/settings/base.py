bc3a8aca7f65473ee44b63bf2783e2af2a83273a
John Stinson <john@leadsherpa.com>
2023-08-23 17:01:13 -0500

    New user profile

--- config/settings/base.py
+++ config/settings/base.py
@@ -131,3 +131,5 @@ STATIC_ROOT = "interview/static"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "profiles.UserProfile"
